b60c47f2d6c2300f48bebe1bcd55d7514be248d8
Aziz <sha.milov@mail.ru>
2024-08-31 19:52:53 +0400

    finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,116 +1,106 @@
 import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProgressBar from './ProgressBar';
 import Upgrades from './UpgradesBox';
-import Img1 from './Img1.png'
-import Img2 from './Img2.png'
-import Img3 from './Img3.png'
-import Img4 from './Img4.png'
-import Img5 from './Img5.png'
+import Img1 from './Img1.png';
+import Img2 from './Img2.png';
+import Img3 from './Img3.png';
+import Img4 from './Img4.png';
+import Img5 from './Img5.png';
 
 function App() {
+  const [show, setShow] = useState(false);
   const [count, setCount] = useState(0);
   const [HP, setHP] = useState(10);
   const [Level, setLevel] = useState(0);
-  const [textColor, setColor] = useState('#11111')
-  const [Img, setImage] = useState(0)
+  const [textColor, setColor] = useState('#111111');
+  const [Img, setImage] = useState(0);
   const [clickPosition, setClickPosition] = useState(null);
   const [showFlicker, setShowFlicker] = useState(false);
-  const ClickerImage = [Img1,Img2,Img3,Img4,Img5];
-  const [ClickingPowerUpgrade, setClickingPowerUpgrade] = useState(1);
-  
+  const [ClickingPowerUpgrade, setClickingPowerUpgrade] = useState(0);
+  const ClickerImage = [Img1, Img2, Img3, Img4, Img5];
 
-  
-  function randDarkColor() {
-    var lum = -0.25;
-    var hex = String('#' + Math.random().toString(16).slice(2, 8).toUpperCase()).replace(/[^0-9a-f]/gi, '');
+  const handleClick2 = () => {
+    setShow(prevShow => !prevShow); // Toggle visibility
+  };
+
+  const randDarkColor = () => {
+    const lum = -0.25;
+    let hex = String('#' + Math.random().toString(16).slice(2, 8).toUpperCase()).replace(/[^0-9a-f]/gi, '');
     if (hex.length < 6) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
     }
-    var rgb = "#",
-    c, i;
-    for (i = 0; i < 3; i++) {
-    c = parseInt(hex.substr(i * 2, 2), 16);
-    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-    rgb += ("00" + c).substr(c.length);
+    let rgb = "#";
+    for (let i = 0; i < 3; i++) {
+      let c = parseInt(hex.substr(i * 2, 2), 16);
+      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+      rgb += ("00" + c).substr(c.length);
     }
     return rgb;
-    }
+  };
 
   const handleClick = (event) => {
-    setCount(prevCount => prevCount + ClickingPowerUpgrade);
-    setHP(prevHP => prevHP - 0.5); // Increase HP when button is clicked
+    setCount(prevCount => prevCount + 1 + ClickingPowerUpgrade);
+    setHP(prevHP => prevHP - 0.5);
     const { clientX, clientY } = event;
     setClickPosition({ x: clientX, y: clientY });
     setShowFlicker(true);
-    setTimeout(() => {
-      setShowFlicker(false);
-    }, 300);   
-    
+    setTimeout(() => setShowFlicker(false), 300);
   };
+
   useEffect(() => {
     const randomColor = randDarkColor();
     document.documentElement.style.setProperty('--main-bg-color', randomColor);
-    setImage(Img+1)
-    if (Img === 4){
-      setImage(0)
-    };
+    setImage(prevImg => (prevImg + 1) % 5);
   }, [Level]);
-  
-  const UpgradeClickingPower = () =>{setClickingPowerUpgrade(ClickingPowerUpgrade+5)};
 
- 
-  // Determine text color based on count
-  if (HP === 0){
-    
-    setHP(10);
-    setLevel(Level+1);
-    console.log(Level);
-    setColor(`#${Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0')}`)
-    // TODO : Add upgrades like "chopsticks" + 5 clicking power, try to make some kind of enlargening animation OnClick, some kind of level boost, and use cookies to save progress of USER 
-  };
+  useEffect(() => {
+    if (HP <= 0) {
+      setHP(10);
+      setLevel(prevLevel => prevLevel + 1);
+      setColor(`#${Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0')}`);
+    }
+  }, [HP]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-        </div>
         <div className="counter" style={{ color: textColor }}>
-        
           <h1>{count}</h1>
           <ProgressBar HP={HP} Level={Level} />
+          {show && (
+            <Upgrades
+              count={count}
+              setUpgrade1={setClickingPowerUpgrade}
+              Upgrade1={ClickingPowerUpgrade}
+              show={show}
+              setShow={setShow}
+              handleClick2={handleClick2}
+              setCount={setCount}
+              Level={Level}
+            />
+          )}
           <div className='button-container'>
-            <img className='SushiImg' onClick={handleClick} src={ClickerImage[Img]}/>
+            <img className='SushiImg' onClick={handleClick} src={ClickerImage[Img]} alt="Clicker" />
+            <button onClick={handleClick2} className='button'>Show Upgrades</button>
           </div>
           {clickPosition && (
-        <div
-          className={showFlicker ? 'flicker' : ''}
-          style={{
-            position: 'absolute',
-            left: clickPosition.x,
-            top: clickPosition.y,
-            transform: 'translate(-50%, -50%)',
-            color: 'white',
-            borderRadius: '5px',
-            pointerEvents: 'none', 
-            opacity : '0%',
-          }}
-        >
-          +1
-        </div>
-      )}
-          <h4>UPGRADES BOX </h4>
-
-        </div>
-        <div className='upgrades-container'> 
-
-
-          
-          <Upgrades Upgrade1={ClickingPowerUpgrade} setUpgrade1={setClickingPowerUpgrade}/>
-
-        
-
+            <div
+              className={showFlicker ? 'flicker' : ''}
+              style={{
+                position: 'absolute',
+                left: clickPosition.x,
+                top: clickPosition.y,
+                transform: 'translate(-50%, -50%)',
+                color: 'white',
+                borderRadius: '5px',
+                pointerEvents: 'none',
+                opacity: '0%',
+              }}
+            >
+              +{ClickingPowerUpgrade + 1}
+            </div>
+          )}
         </div>
       </header>
     </div>
--- my-app/src/UpgradesBox.js
+++ my-app/src/UpgradesBox.js
@@ -1,39 +1,122 @@
+import { useEffect, useState } from "react";
 
-
-function Upgrades({Upgrade1, setUpgrade1}) {
-   
-
-   const handleClick = () =>{setUpgrade1(Upgrade1+5)};
-   
-    
-    return (
-      <div className="upgrades">
-        <span className="Upgrade-1"> <div
-        style={{
-        
-        transform:"scale(1.3)",
-        display: "table",
-        justifycontent: "center",
-       
-        height: "15%",
-        margin: "0 auto",
-        padding:"60px",
-      }}
-        >
-           <div
-        style={{marginBottom: "10px", border: `5px solid black`,padding: "20px"}} onClick={handleClick}
-           > <p className="UpgradesText"> Click Power +5 LVL :{Upgrade1}  </p> </div>
-           <div
-        style={{ marginBottom: "10px", border: `5px solid black`,padding: "20px"}}
-           ><p className="UpgradesText"> Auto Clicker +1 LVL : </p></div>
-           <div
-        style={{border: `5px solid black`,padding: "20px"}}
-           ><p className="UpgradesText"> Coming soon... </p></div>
-         </div> </span>
-            </div>
-    );
-  }
+function Upgrades({ Upgrade1, setUpgrade1, count, handleClick2, setCount, Level }) {
+  const [costUpgrade, setCostUpgrade] = useState(45);
+  const [costUpgrade2, setCostUpgrade2] = useState(500);
+  const [autoClickerStatus, setAutoClickerStatus] = useState(false);
   
 
+  const handleClick = () => {
+    if (count >= costUpgrade) {
+      setUpgrade1(prevUpgrade1 => prevUpgrade1 + 1);
+      setCount(prevCount => prevCount - costUpgrade);
+      console.log(costUpgrade);
+    }
+  };
+
+  useEffect(() => {
+    if (Level > 10) {
+      setCostUpgrade(300);
+    }
+  }, [Level]);
+
+  const handleClick3 = () => {
+    if (count >= costUpgrade2) {
+      setCount(prevCount1 => prevCount1 - costUpgrade2);
+      console.log(costUpgrade2);
+      setAutoClickerStatus(true); // Start the auto-clicker
+     
+    }
+  };
+
+  const autoClicker = () => {
+    if (autoClickerStatus) {
+      setCount(prevCount => prevCount + 1); // Increment count by 2
+      setTimeout(autoClicker, 1000); // Repeat after 2 seconds
+    }
+  };
+
+  useEffect(() => {
+    if (autoClickerStatus) {
+      autoClicker(); // Start auto-clicker when activated
+    }
+  }, [autoClickerStatus]);
+
+  return (
+    <div className='ShowUpgrades'>
+      <span className="Upgrade-1">
+        <div
+          style={{
+            width: "600px",
+            transform: "scale(0.9)",
+            margin: "auto",
+          }}
+        >
+          <div
+            style={{
+              marginBottom: "10px",
+              border: `10px solid black`,
+              padding: "0px",
+              marginTop: "20px",
+              backgroundColor: "black",
+            }}
+            onClick={handleClick}
+          >
+            <p className="UpgradesText">
+              Click Power +5
+              <small>
+                <small>
+                  <small>
+                    <small>
+                      <small>per click</small>
+                    </small>
+                  </small>
+                </small>
+              </small>
+              <br />
+              LVL : {Upgrade1}
+              <span style={{ fontSize: "30px", opacity: "30%" }}>
+                <br />
+                Price: {costUpgrade}
+              </span>
+            </p>
+          </div>
+          <div
+            style={{
+              marginBottom: "10px",
+              border: `10px solid black`,
+              padding: "0px",
+              backgroundColor: "black",
+            }}
+            onClick={handleClick3}
+          >
+            <p className="UpgradesText">
+              Auto Clicker +1
+              
+              <br /><small>
+                <small>
+                  <small>
+                    <small>
+                      <small>per second</small>
+                    </small>
+                  </small>
+                </small>
+              </small>
+              
+              <span style={{ fontSize: "30px", opacity: "30%" }}>
+                <br />
+                Price: {costUpgrade2}
+              </span>
+            </p>
+          </div>
+
+          <button className="button" onClick={handleClick2}>
+            Hide Upgrades
+          </button>
+        </div>
+      </span>
+    </div>
+  );
+}
 
 export default Upgrades;
